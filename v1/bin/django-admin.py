258f88b92f6d3e0195151ddf7f3fc850b1e3aaf0
debasis nath <debasisnath85@gmail.com>
2019-09-26 11:37:53 +0530

    changed the venv

--- v1/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/elegant/Desktop/clgproject/backend/v1/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- v1/bin/pwhich.py
+++
@@ -1,9 +0,0 @@
-#!/home/elegant/Desktop/clgproject/backend/v1/bin/python3
-import os
-import sys
-imported_module = __import__(sys.argv[1])
-try:
-    print os.path.abspath(os.path.dirname(imported_module.__file__))
-except AttributeError:
-    print 'stdlib'
-

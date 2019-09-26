8c6843565a35975856c163b273e83e7d33c0e525
debasis nath <debasisnath85@gmail.com>
2019-09-26 22:39:43 +0530

    finished the backned part

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, RegexValidator, MinValueValidator
 
 
 class Customer(models.Model):
@@ -6,10 +7,16 @@ class Customer(models.Model):
     name = models.CharField(max_length=20)
     address = models.TextField()
     mailid = models.EmailField()
-    contact = models.PositiveIntegerField()
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{10}$', message="Phone: 10 digits allowed.")
+
+    contact = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
+
     lastservice = models.DateField()
     investment = models.FloatField(default=0)
-    age = models.IntegerField(default=0)
+    age = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(60), MinValueValidator(18)], error_messages={'validators': 'Age greater than 60 and less than 18 is not allowed'})
     gender = models.CharField(
         max_length=9, choices=GENDER_CHOICES, default='1')
 
